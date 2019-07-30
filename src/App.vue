9890741429d7cebd08e9b62bcd0011024ab3ae92
Jackson <jackson.rodrigues9@hotmail.com>
2019-07-30 16:03:15 -0300

    Deploy com o readme

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+	<div id="app">
+
+		<span>
+			<button @click="component = 'Citacoes'"> Citações </button>
+			<button @click="component = 'Sobre'"> Sobre </button>
+		</span>
+		<!-- Serve para não resetar o componente quando trocar em si -->
+	<keep-alive>
+		<component :is="component"></component>
+	</keep-alive>
+		
+		<!-- <Citacoes />
+		<Sobre /> -->
+	</div>
+</template>
+
+<script>
+import Citacoes from './components/Citacoes'
+import Sobre from './components/Sobre'
+
+export default {
+	components: { Citacoes, Sobre },
+	data() {
+		return {
+			component: 'Citacoes'
+		}
+	}
+}
+</script>
+
+<style>
+	#app {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		font-size: 1.6rem;
+	}
+
+	button {
+        outline: none;
+        padding: 5px 15px 10px;
+        margin: 10px 5px;
+        border-radius: 3px;
+        font-size: 2rem;
+        font-weight: 300;
+        color: #FFF;
+        background-color: rgba(0, 0, 0, .5);
+    }
+</style>
---
+++ src/components/Citacoes.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="citacoes">
+        <span>
+            <button @click="numero--">&lt;</button>
+            <button @click="numero++">&gt;</button>
+        </span>
+
+        <citacao>
+            <h1 slot="autor">Autor:</h1>
+            <h1 slot="autor">{{citacoes[indice].autor}}</h1>
+            <p>Texto:</p>
+            <p>{{citacoes[indice].texto}}</p>
+            <h6 slot="fonte">Fonte:</h6>
+            <h6 slot="fonte">{{citacoes[indice].fonte}}</h6>
+        </citacao>
+    </div>
+</template>
+
+<script>
+import Citacao from './Citacao'
+
+export default {
+    components: {
+        Citacao
+    },
+    data() {
+        return {
+            numero: 0,
+            citacoes: [{
+                fonte: 'Jornal do Empreendedor',
+                texto: 'Lembre-se sempre que você é absolutamente único. Assim como todos os outros.',
+                autor: 'Margaret Mead'
+            }, { 
+                fonte: 'Frases de Mãe',
+                texto: 'Isso não é um quarto, é um chiqueiro.',
+                autor: 'Roberta'
+            }, {
+                fonte: 'Frases de Pai',
+                texto: 'Vou contar até 3! 1, 2, 2...',
+                autor: 'Gustavo'
+            }]
+        }
+    },
+    computed: {
+        indice() {
+            return Math.abs(this.numero % 3);
+        }
+    },
+    activated() {
+        console.log('activated')
+    },
+    deactivated() {
+        console.log('deactivated')
+    }
+}
+</script>
+
+<style scoped>
+    .citacoes {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h1 {
+        color: green
+    }
+</style>
